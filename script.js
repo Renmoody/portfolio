f1f51f46131aa6b009d3b5ca60937209bfd11d61
Ren <ren@pop-os.mynetworksettings.com>
2026-02-17 09:48:38 -0500

    Text cleanup

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Select all project containers
 const projects = document.querySelectorAll('.project');
 
 projects.forEach(project => {
     project.addEventListener('click', () => {
-        // This updates the URL to #projectid, triggering the CSS :target
         window.location.hash = project.id;
     });
 });
