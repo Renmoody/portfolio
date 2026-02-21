010f0e512f846dd13c887f179d6f312dfc26fb56
Ren <ren@pop-os.mynetworksettings.com>
2026-02-21 16:49:13 -0500

    Added a lot of interactivity with JS, cleanup file structure

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 const projects = document.querySelectorAll('.project');
 
-projects.forEach(project => {
-    project.addEventListener('click', (e) => {
-        if (e.target.closest('.video-container')) {
-            return; 
+
+document.querySelectorAll('.project').forEach(project => {
+    project.addEventListener('click', () => {
+        const skills = project.getAttribute('data-skill');
+        if (skills) {
+            const skillArray = skills.split(' ');
+            skillArray.forEach(skillName => {
+                const tag = document.querySelector(`.skill-tag[data-skill="${skillName}"]`);
+                if (tag) tag.classList.add('active-skill');
+            });
         }
-        window.location.hash = project.id;
+        project.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    });
+
+    project.addEventListener('mouseenter', () => {
+        const skills = project.getAttribute('data-skill');
+        if (skills) {
+            const skillArray = skills.split(' ');
+            skillArray.forEach(skillName => {
+                const tag = document.querySelector(`.skill-tag[data-skill="${skillName}"]`);
+                if (tag) tag.classList.add('active-skill');
+            });
+        }
+    });
+
+
+    project.addEventListener('mouseleave', () => {
+        document.querySelectorAll('.skill-tag').forEach(tag => {
+            tag.classList.remove('active-skill');
+        });
     });
 });
+const skillsSection = document.querySelector('#skills-overview');
+
+const observer = new IntersectionObserver(
+    ([e]) => e.target.classList.toggle('is-pinned', e.intersectionRatio < 1),
+    { threshold: [1] }
+);
+
+observer.observe(skillsSection);
